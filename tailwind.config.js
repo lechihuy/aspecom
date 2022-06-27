31977e791bbe8fcc145092a8f37735890e6b7383
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-06-27 16:39:30 +0700

    vendor: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Views/**/*.cshtml",
+    "./Areas/**/*.cshtml"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
